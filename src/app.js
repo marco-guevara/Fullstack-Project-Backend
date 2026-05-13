496291b24c1fc50b00a63cfd4271921e90880560
Marco Guevara <marcojgh352@gmail.com>
2026-05-13 17:53:55 +0200

    add: cors

--- src/app.js
+++ src/app.js
@@ -1,13 +1,16 @@
+import cors from "cors";
 import express from "express";
 import authRoutes from "./routes/authRoutes.js";
 import cartRoutes from "./routes/cartRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
+import corsOptions from "./config/cors.js";
 import { protect } from "./middlewares/authMiddleware.js";
 import { notFound } from "./middlewares/notFoundMiddleware.js";
 
 const app = express();
 
+app.use(cors(corsOptions));
 app.use(express.json());
 
 app.use("/auth", authRoutes);
